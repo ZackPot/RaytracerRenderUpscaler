946de2b9485f14f5c85f4215df95031c223e366a
Zachary Potishko <130048004+ZackPot@users.noreply.github.com>
2026-05-03 19:40:11 +0100

    Added the neural network to model.py.

--- model.py
+++ model.py
@@ -1,35 +1,43 @@
 import os
 from PIL import Image
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
 
-# Low Quality Image Extraction
-target_letter = 'l'
-root_dir = './images/'
-lq_filenames = []
-lq_images = []
+def img_extract(target_letter):
+    print('LQ')
+    root_dir = './images/'
+    filenames = []
+    images = []
 
-for root, dirs, files in os.walk(root_dir):
-    for filename in files:
-        if filename.lower().startswith(target_letter.lower()):
-            lq_filenames.append(os.path.join(root, filename))
+    for root, dirs, files in os.walk(root_dir):
+        for filename in files:
+            if filename.lower().startswith(target_letter.lower()):
+                filenames.append(os.path.join(root, filename))
 
-for image in lq_filenames:
-    img = Image.open(image)
-    lq_images.append(np.array(img).reshape(-1))
+    for image in filenames:
+        img = np.array(Image.open(image))
+        img = img.astype('float32') / 255.0
+        images.append(img.reshape(-1))
 
-# High Quality Image Extraction
-target_letter = 'h'
-root_dir = './images/'
-hq_filenames = []
-hq_images = []
+    return images
 
-for root, dirs, files in os.walk(root_dir):
-    for filename in files:
-        if filename.lower().startswith(target_letter.lower()):
-            hq_filenames.append(os.path.join(root, filename))
+X = img_extract('l')
+y = img_extract('h')
 
-for image in lq_filenames:
-    img = Image.open(image)
-    hq_images.append(np.array(img).reshape(-1))
+try:
+    model = tf.keras.models.Sequential([
+        layers.Dense(64, activation='relu'),
+        layers.Dropout(0.4),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(len(y[0]), activation='sigmoid'),
+    ])
 
-print(hq_images[0])
+    model.compile(optimizer='adam', loss='mme', metrics=['accuracy'])
+
+    tf.keras.utils.plot_model(model, to_file='model.png', show_shapes=True)
+    model.fit(X, y, epochs=10)
+    model.save('upscaler.h5')
+except Exception as e:
+    print(e)
+    print('Most likely cause is that image resolutions are different.')
